77cbeaeacf64df350c62ead0645fcb418401fc5d
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-09-22 09:43:57 -0400

    add exercise #10 Ruby Tutorial

---
+++ ex10.rb
@@ -0,0 +1,6 @@
+puts "Enter your Name:"
+name = gets.chomp
+puts "Enter your age:"
+age = gets.chomp
+puts "Hello #{name}, Im happy you have reached #{age} years old"
+# This explains the basics of using gets.chomp
