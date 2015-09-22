0a5c594eb073f0698d3e3591a3376fba37d48314
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-09-21 22:02:06 -0400

    added Exercise #7

---
+++ ex7.rb
@@ -0,0 +1,28 @@
+a =  "Marry had a little Lamb."
+# n = "Snow"
+b = "Its fleece was white as #{'Snow'}."
+c = "And everywhere that Marry went. "
+t =  "." * 10 # This will print 10 dots
+
+puts a
+puts b
+puts c
+puts t
+
+end1 = 'C'
+end2 = 'H'
+end3 = 'E'
+end4 = 'E'
+end5 = 'S'
+end6 = 'E'
+end7 = 'B'
+end8 = 'U'
+end9 = 'R'
+end10 = 'G'
+end11 = 'E'
+end12 = 'R'
+
+# print command does not create a nextline
+print  end1 + end2 + end3 + end4 + end5 + end6 
+# puts command does create a nextline
+puts end7 + end8 + end9 + end10 + end11 + end12   
