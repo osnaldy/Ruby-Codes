f3d0f84ef016940ef7ad9b08e9969e2f639be283
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-09-21 22:01:02 -0400

    added Exercise #6

---
+++ ex6.rb
@@ -0,0 +1,19 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people." #This is a string with a string in it.
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}." #This is a string with a string in it.
+
+puts x
+puts y
+
+puts "i said: #{x}."
+puts "i also said '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny? #{hilarious}." #This is a string with a string in it.
+puts joke_evaluation
+w = "This is the left side of.... "
+e = "a string with a right side."
+
+puts w + e
