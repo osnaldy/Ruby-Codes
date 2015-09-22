9b52af5479c42721575af75ec86d89fc04d502d5
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-09-21 22:00:01 -0400

    add Exercise #5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+name = 'Osnaldy Vasquez'
+age = 25
+height = 5.9
+kg = 2.2
+weight = (200 / kg).round(2)
+eyes = 'black'
+teeth = 'white'
+hair =  'black'
+
+puts "let's talk about #{name}"
+puts "he is #{age}"
+puts "he's #{weight}"
+puts "actually I'm very heavy"
+puts "i have #{eyes} eyes and my hair is #{hair} "
+puts "My teeth are #{teeth}"
+puts "if you add #{age}, #{height}, and #{weight} i get #{age + height + weight}."
