9feac7ec98b52ccaf204494be6f2600b24f7dea5
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-09-23 19:42:24 -0400

    add Excercise #14 Ruby Tutorial

---
+++ ex14.rb
@@ -0,0 +1,19 @@
+user_name = ARGV.first
+prompt = '> '
+
+puts "Hi #{user_name}."
+puts "I would like to ask you a few questions."
+puts "do you like me #{user_name}?", prompt
+likes = $stdin.gets.chomp
+
+puts "Where do you live #{user_name}?", prompt
+lives = $stdin.gets.chomp
+
+puts "what kind of computer do you have #{user_name}?", prompt
+computer =  $stdin.gets.chomp
+
+puts """
+Alright, so you said #{likes} about liking me.
+You live in #{lives}.  Not sure where that is.
+And you have a #{computer} computer.  Nice.
+"""
