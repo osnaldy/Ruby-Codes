887e5f3db157acd0b58e5e63f7f6bbc64b6357a0
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-01 15:19:04 -0400

    Calculate hours in year, minutes in a decade, and Age in seconds

--- calc.rb
+++ calc.rb
@@ -10,4 +10,10 @@ puts "Per year we have #{hour_per_year}"
 puts "If each #{hDay} hours has 60 minutes"
 min_per_year = hour_per_year * 60
 puts "A Year we have #{min_per_year} minutes"
-puts "In a decade we have #{min_per_year * 10} minutes"
+min_in_decade = min_per_year * 10
+puts "In a decade we have #{min_in_decade} minutes"
+
+puts "Enter your age:"
+age = gets.chomp.to_i
+puts "Your age in second is #{(min_per_year * 60) * age} seconds alive"
+
