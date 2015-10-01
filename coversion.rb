29e9b394424e69d266f3e49c74b00a3c9b571576
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-01 19:18:19 -0400

    Examples using .to_i, .to_s, .to_f

---
+++ coversion.rb
@@ -0,0 +1,12 @@
+var1 = 2
+var2 = '5'
+puts var1.to_s + var2
+puts var1 + var2.to_i
+
+puts '15'.to_f
+puts '99.999'.to_i
+puts '56 is my favorite number!'.to_i
+puts 'stringy'.to_i
+puts 20
+puts 20.to_s
+puts '20'
