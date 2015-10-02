16078ae863dfa2c61c8b6529f4ff6d64df85222c
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-01 20:57:43 -0400

    Exercise greeting a person

---
+++ chomp.rb
@@ -0,0 +1,13 @@
+puts "Hello there, and what\'s your Name:"
+n= gets.chomp
+puts 'Your name is ' + n + '? What a lovely name!'
+puts 'Please to meet you, ' + n + '. :)'
+
+puts "Enter the first name:"
+first_name = gets.chomp
+puts "Enter your middle name:"
+middle_name = gets.chomp
+puts "Enter Your last name:"
+last_name = gets.chomp
+
+puts 'Hey ' + first_name + ' ' + middle_name + ' ' + last_name + ' is very nice to meet you'
