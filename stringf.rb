0db5687158232191272b56239ef4cd407c550f10
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-04 23:25:42 -0400

    Reverse, and length of two strings

---
+++ stringf.rb
@@ -0,0 +1,27 @@
+puts ('hello ' + 'world')
+puts ((10 * 9) + 9)
+puts self
+
+var1 = 'stop'
+var2 = 'deliver repaid desserts'
+var3 = '....TCELES B HSUP A magic spell?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+
+puts var1
+puts var2
+puts var3
+
+puts 'What is your full name?'
+name = gets.chomp
+puts 'did you know there are ' + name.length.to_s + ' characters'
+puts 'in your name, ' + name  + '?'
+
+puts 'Enter your firstname:'
+f_name = gets.chomp
+puts 'Enter your last name:'
+l_name = gets.chomp
+length_total = f_name + l_name
+puts 'The length of your name is ' + length_total.length.to_s + '!'
