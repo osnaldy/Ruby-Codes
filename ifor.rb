65de94fc340ae942c6466b689dc3d3f6c71ff459
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-11 19:50:34 -0400

    elseif

---
+++ ifor.rb
@@ -0,0 +1,9 @@
+puts 'Hello, What\'s your name ?'
+name = gets.chomp
+puts 'Hello, ' + name.capitalize + '.'
+
+if name == 'Osnaldy'.downcase
+	puts 'Awesome name ' + name.capitalize + '.'
+elsif name == 'Ostempo'.downcase 
+		puts 'Awesome name ' + name.capitalize + '.'
+end
