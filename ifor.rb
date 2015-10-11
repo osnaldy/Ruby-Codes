7f9089732856223bb8c9d2a037d448aba892245f
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-11 19:54:29 -0400

    use of ||(or)

--- ifor.rb
+++ ifor.rb
@@ -2,8 +2,6 @@ puts 'Hello, What\'s your name ?'
 name = gets.chomp
 puts 'Hello, ' + name.capitalize + '.'
 
-if name == 'Osnaldy'.downcase
-	puts 'Awesome name ' + name.capitalize + '.'
-elsif name == 'Ostempo'.downcase 
+if name == 'Osnaldy'.downcase || name == 'Ostempo'.downcase
 		puts 'Awesome name ' + name.capitalize + '.'
 end
