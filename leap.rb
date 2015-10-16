641a6a7b52b71bb86f99d0abe9307eb43d5960c1
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-16 11:24:50 -0400

    Print the leap years between a range

---
+++ leap.rb
@@ -0,0 +1,20 @@
+puts 'Enter the start year'
+start = gets.chomp
+puts 'Enter the end year'
+finish = gets.chomp
+
+if finish.to_i < start.to_i
+	puts 'The end year should be bigger than start year'
+	puts 'Re-enter the start year'
+	start = gets.chomp
+	puts 'Re-enter the end year'
+	finish = gets.chomp
+else
+	while start.to_i < finish.to_i
+		while (start.to_i % 4 == 0 && start.to_i % 100 != 0) || (start.to_i % 400 == 0)
+			puts start.to_s + ' It is a leap'
+			start = start.to_i + 1
+		end
+			start = start.to_i + 1
+	end
+end
