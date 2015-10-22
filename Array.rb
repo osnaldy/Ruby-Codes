d24883f15e7f81892dbf9e38b8a7dd92a1000d14
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-21 22:30:42 -0400

    push, pop and last

--- Array.rb
+++ Array.rb
@@ -25,7 +25,26 @@ puts
 puts foods.join(', ')
 puts 
 puts foods.join('  :)   ') + '	 8)'
+puts
 
 200.times do
 	puts []
 end
+
+favorites = []
+favorites.push 'raindrops on roses'
+favorites.push 'Hello word'
+favorites.push 'Ruby is Fun'
+
+puts favorites[0]
+puts favorites[1]
+puts favorites.last
+puts favorites.length
+puts favorites.pop	
+puts favorites
+puts favorites.length
+
+
+
+
+
