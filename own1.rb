7e76c8f765bc789481c0644a2696c44f0b2b8123
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-10-26 20:56:44 -0400

    .

--- own1.rb
+++ own1.rb
@@ -2,23 +2,19 @@ def ask question
 	while true
 		puts question
 		reply = gets.chomp.downcase
-
-		if (reply == 'yes' || reply == 'no')
+		if reply == 'yes' || reply == 'no'
 			if reply == 'yes'
-				answer = true
-			else
-				answer = false
+				return true
+			elsif reply == 'no'
+				return false
 			end
 			break
 		else
-			puts 'Please answer "yes" or "no"'
+			puts 'You must answer "yes" or "no"'
 		end
 	end
-	answer # This is what we return (true or false)
+	return true || false
 end
-
-puts 'Hello, and thank you for......'
-puts
 
 ask 'Do you like eating tacos?'
 ask 'Do you like eating burritos?'
@@ -28,3 +24,9 @@ ask 'Do you like eating sopapillas?'
 puts 'Just a few more questions...'
 ask 'Do you like drinking horchata?'
 ask 'Do you like eating flautas?'
+
+puts
+puts 'DEBRIEFING'
+puts 'Thank you for....'
+puts
+puts wets_bed
