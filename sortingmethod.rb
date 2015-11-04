d631da4d1d4430ba45e8b661e9b4772bad101460
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-11-03 22:47:27 -0500

    unfinished sort

---
+++ sortingmethod.rb
@@ -0,0 +1,22 @@
+def sort some_array
+	recursive_sort some_array, []
+end
+
+def recursive_sort unsorted_array, sorted_array
+	#all recursive function return when 
+	#they reach the base case
+
+	if unsorted_array.length == 0
+		return
+		
+		#check all the element of the unsorted array 
+		#and push the smallest value to the end of 
+		#the sorted array
+	else 
+		return unsorted_array		
+	end
+end
+
+arr = [2,4,5,1]
+
+puts sort arr
