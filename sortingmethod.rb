124b4bb3b1eb4b6485915cf757dc7ab6d5115e29
Osnaldy Vasquez <osnaldy@hotmail.com>
2015-11-05 22:42:06 -0500

    ...

--- sortingmethod.rb
+++ sortingmethod.rb
@@ -7,16 +7,33 @@ def recursive_sort unsorted_array, sorted_array
 	#they reach the base case
 
 	if unsorted_array.length == 0
-		return
+		return sorted_array
 		
 		#check all the element of the unsorted array 
 		#and push the smallest value to the end of 
 		#the sorted array
 	else
+		#minor = unsorted_array[0]
+		for i in 0..unsorted_array.size - 2
+			minor = i
+			for j in i+1..unsorted_array.size - 1
+				minor = j if unsorted_array[j] < unsorted_array[minor]
+			end
+			sorted_array << unsorted_array[minor]
+			unsorted_array[i], unsorted_array[minor] = unsorted_array[minor], unsorted_array[i]
+		
+			if unsorted_array[i] < minor
+			minor = unsorted_array[i]
+			end
+			return minor
+		end
+
+		#puts sorted_array
+		puts "unsorted"
 		return unsorted_array
 	end
 end
 
-arr = ['osnaldy',4,5,1]
-
-puts sort arr
+arr = [9,5,1,4,7]
+sort arr
+puts arr
